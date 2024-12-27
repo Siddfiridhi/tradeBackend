17d7af1c00e2f9edad2affca9451bdc22c6e0dc5
Siddfiridhi <sidd.finridhi@gmail.com>
2024-12-27 23:24:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,8 @@ mongoose
 app.use('/api/users', userRoutes);
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
